372dbe2e1aeb6ae49ca07444b7ef531c0407f141
Kwane Smith <smithkwane6@gmail.com>
2020-02-19 04:55:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,4 +27,12 @@ class Artist
     @all
   end
 
+  def self.find_or_create_by_name(name)
+    self.all.detect {|artist| artist.name == name} || Artist.new(name).save
   end
+
+  def print_songs
+    self.songs.each {|song| puts song.name}
+  end
+
+end
