b6f68e6deb52ab5b4c137a6ed272a9856c87f969
Kwane Smith <smithkwane6@gmail.com>
2020-02-19 05:02:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,10 +5,10 @@ class Song
     @name = name
   end
 
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end
-  
+
   def self.new_by_filename(file)
     artist_name = file.split(" - ")[0]
     song_name = file.split(" - ")[1]
